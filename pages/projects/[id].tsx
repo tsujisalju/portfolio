85947d231e9391bedacd00667f221efe009edeb4
Qayyum Yazid <smolpadok@protonmail.com>
2022-08-19 13:12:28 +0200

    testing twitter meta

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -4,6 +4,7 @@ import { getAllProjectIds, getProjectData } from "../../lib/projects";
 import { GetStaticProps, GetStaticPaths } from "next";
 import Date from "../../components/Date";
 import Image from "next/image";
+import Head from "next/head";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const projectData = await getProjectData(params?.id as string);
@@ -35,38 +36,49 @@ export default function Project({
     contentHtml: string;
   };
 }) {
+  const paragraphs = projectData.contentHtml.split("</p>");
+  const excerpt = paragraphs[0] + "</p>";
   return (
-    <Layout>
-      <div className="flex flex-col lg:flex-row lg:space-x-4 justify-center">
-        <div>
-          <Image
-            className="transition duration-500 ease-in-out"
-            alt={projectData.title}
-            src={projectData.img}
-            width={projectData.width}
-            height={projectData.height}
-            placeholder="blur"
-            blurDataURL={projectData.img.replace(".", "-placeholder.")}
-          />
-        </div>
-        <div>
-          <div className="flex flex-col p-8 space-y-4 bg-black lg:w-[500px]">
-            <Link href="/">
-              <a className="font-sans">Back to home</a>
-            </Link>
-            <div className="flex flex-col space-y-2 mb-4 text-stone-200">
-              <h1 className="font-serif text-5xl lg:text-6xl ">
-                {projectData.title}
-              </h1>
-              <Date dateString={projectData.date} />
-            </div>
-            <div
-              className="flex flex-col space-y-4 text-lg text-stone-200"
-              dangerouslySetInnerHTML={{ __html: projectData.contentHtml }}
+    <>
+      <Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@smolpadok" />
+        <meta name="twitter:title" content={projectData.title} />
+        <meta name="twitter:description" content={excerpt} />
+        <meta name="twitter:image" content={projectData.img} />
+      </Head>
+      <Layout>
+        <div className="flex flex-col lg:flex-row lg:space-x-4 justify-center">
+          <div>
+            <Image
+              className="transition duration-500 ease-in-out"
+              alt={projectData.title}
+              src={projectData.img}
+              width={projectData.width}
+              height={projectData.height}
+              placeholder="blur"
+              blurDataURL={projectData.img.replace(".", "-placeholder.")}
             />
           </div>
+          <div>
+            <div className="flex flex-col p-8 space-y-4 bg-black lg:w-[500px]">
+              <Link href="/">
+                <a className="font-sans">Back to home</a>
+              </Link>
+              <div className="flex flex-col space-y-2 mb-4 text-stone-200">
+                <h1 className="font-serif text-5xl lg:text-6xl ">
+                  {projectData.title}
+                </h1>
+                <Date dateString={projectData.date} />
+              </div>
+              <div
+                className="flex flex-col space-y-4 text-lg text-stone-200"
+                dangerouslySetInnerHTML={{ __html: projectData.contentHtml }}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
