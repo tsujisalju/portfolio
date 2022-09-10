921bf6b05bf40e2ede00c4c7d0e6e56cda3a00a5
Qayyum Yazid <smolpadok@protonmail.com>
2022-09-10 21:48:28 +0200

    update almanac

--- src/pages/almanac/index.tsx
+++ src/pages/almanac/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="flex flex-col items-center space-y-4">
           <h1 className="font-serif text-5xl">Almanac</h1>
           <p className="font-sans text-lg">
-            I know you&apos;re excited, but it&apos;s work in progress
+            Get to know the members of the consortium.
           </p>
         </div>
       </div>
