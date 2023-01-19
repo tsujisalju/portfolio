98b2a1b5a786b724583564667776727c3bc5165e
Qayyum Yazid <smolpadok@protonmail.com>
2023-01-19 12:27:53 +0800

    remove fade in due to bug

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
 import { ReactNode } from "react";
-import { useIntl } from "react-intl";
 import { FadeIn } from "../utilities/FadeIn";
 
 export default function Hero({ children }: { children: ReactNode }) {
-  const intl = useIntl();
   return (
-    <FadeIn>
-      <div className="container m-auto max-w-5xl flex flex-1 flex-col p-16 xl:px-0 space-y-4">
-        {children}
-      </div>
-    </FadeIn>
+    <div className="container m-auto max-w-5xl flex flex-1 flex-col p-16 xl:px-0 space-y-4">
+      {children}
+    </div>
   );
 }
