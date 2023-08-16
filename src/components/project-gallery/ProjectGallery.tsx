b7b0dd93dfde5a28e8a529ca1bc6ff67535bf7e8
purrnama <purrnama@proton.me>
2023-08-16 22:14:45 +0800

    reduce gap space in gallery

--- src/components/project-gallery/ProjectGallery.tsx
+++ src/components/project-gallery/ProjectGallery.tsx
@@ -7,7 +7,7 @@ export default function ProjectGallery({ projects }: { projects: Project[] }) {
   return (
     <section className="px-4">
       <FadeIn>
-        <div className="md:px-8 mx-auto grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-dense items-center">
+        <div className="md:px-8 mx-auto grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-dense items-center">
           {projects.map((project: Project) => (
             <ProjectGalleryItem key={project.id} project={project} />
           ))}
