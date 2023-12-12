af687a92706ffd3aba16b7f7a563f536daf3bb55
purrnama <purrnama@proton.me>
2023-12-12 22:52:44 +0800

    Handle XIVAPI error

--- src/pages/ffxiv.tsx
+++ src/pages/ffxiv.tsx
@@ -79,10 +79,17 @@ export default function FFXIV({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const [isShowing, setIsShowing] = useState(false);
   const intl = useIntl();
-  const xivLevelPercentage =
-    (xivapi.Character.ActiveClassJob.ExpLevel /
-      xivapi.Character.ActiveClassJob.ExpLevelMax) *
-    100;
+  const xivLevelPercentage = () => {
+    if (xivapi.Character) {
+      return (
+        (xivapi.Character.ActiveClassJob.ExpLevel /
+          xivapi.Character.ActiveClassJob.ExpLevelMax) *
+        100
+      );
+    } else {
+      return 0;
+    }
+  };
 
   useEffect(() => {
     document.body.style.backgroundImage = "";
@@ -92,157 +99,180 @@ export default function FFXIV({
 
   return (
     <Layout>
-      <div className="sm:container mx-auto grid lg:grid-cols-2 space-y-4 lg:space-y-0 pb-16">
-        <div className="p-2 sm:p-8">
-          <Transition
-            show={isShowing}
-            enter="transition duration-700"
-            enterFrom="rotate-[30deg] -translate-y-[1000px]"
-            enterTo="-rotate-2 translate-y-0"
-          >
-            <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} tiltReverse scale={1.03}>
-              <Image
-                src={xivapi.Character.Portrait}
-                alt="Player Avatar"
-                width={640}
-                height={873}
-                className="transition rounded-lg shadow-md hover:shadow-xl w-[75%] mx-auto"
-                placeholder="blur"
-                blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                  shimmer(640, 873)
-                )}`}
-                priority
-              />
-            </Tilt>
-          </Transition>
-        </div>
-        <FadeIn>
-          <div className="flex flex-col p-8 lg:pr-16 space-y-8">
-            <div>
-              {xivapi.Character.TitleTop && (
-                <p className="font-sans text-lg">
-                  {xivapi.Character.Title.Name}
-                </p>
-              )}
-              <h1 className="font-display text-4xl">{xivapi.Character.Name}</h1>
-              {!xivapi.Character.TitleTop && (
-                <p className="font-sans text-lg">
-                  {xivapi.Character.Title.Name}
-                </p>
-              )}
-            </div>
-            <div>
-              <div className="flex flex-row space-x-2 items-center pb-2">
+      {xivapi.Character ? (
+        <div className="sm:container mx-auto grid lg:grid-cols-2 space-y-4 lg:space-y-0 pb-16">
+          <div className="p-2 sm:p-8">
+            <Transition
+              show={isShowing}
+              enter="transition duration-700"
+              enterFrom="rotate-[30deg] -translate-y-[1000px]"
+              enterTo="-rotate-2 translate-y-0"
+            >
+              <Tilt
+                tiltMaxAngleX={3}
+                tiltMaxAngleY={3}
+                tiltReverse
+                scale={1.03}
+              >
                 <Image
-                  src={
-                    jobIcons[xivapi.Character.ActiveClassJob.UnlockedState.ID]
-                  }
-                  alt={xivapi.Character.ActiveClassJob.UnlockedState.Name}
-                  height={32}
-                  width={32}
+                  src={xivapi.Character.Portrait}
+                  alt="Player Avatar"
+                  width={640}
+                  height={873}
+                  className="transition rounded-lg shadow-md hover:shadow-xl w-[75%] mx-auto"
+                  placeholder="blur"
+                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                    shimmer(640, 873)
+                  )}`}
+                  priority
                 />
-                <h2 className="font-display text-bold text-2xl">
-                  {xivapi.Character.ActiveClassJob.UnlockedState.Name.toUpperCase()}
-                </h2>
-              </div>
-
-              <h2 className="font-xivmeter text-xl">
-                LEVEL {xivapi.Character.ActiveClassJob.Level}
-              </h2>
-              <div className="h-1 min-w-max bg-stone-300 dark:bg-stone-700">
-                <div
-                  className="h-1"
-                  style={{ width: `${xivLevelPercentage}%` }}
-                >
-                  <Transition
-                    appear={true}
-                    show={isShowing}
-                    enter="origin-left transition duration-[700ms]"
-                    enterFrom="scale-x-0"
-                    enterTo="scale-x-100"
-                  >
-                    <div className="h-1 min-w-min bg-stone-600 dark:bg-stone-300"></div>
-                  </Transition>
-                </div>
-              </div>
-              <p className="font-sans text-md">
-                {xivapi.Character.ActiveClassJob.Level === 90
-                  ? "MAX"
-                  : `EXP ${xivapi.Character.ActiveClassJob.ExpLevel} / ${xivapi.Character.ActiveClassJob.ExpLevelMax}`}
-              </p>
-            </div>
-            <div className="grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6">
+              </Tilt>
+            </Transition>
+          </div>
+          <FadeIn>
+            <div className="flex flex-col p-8 lg:pr-16 space-y-8">
               <div>
-                <h2 className="font-xivmeter text-lg">RACE / CLAN / GENDER</h2>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.Race.Name}
-                </p>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.Tribe.Name}{" "}
-                  {ffxivGender[xivapi.Character.Gender]}
-                </p>
+                {xivapi.Character.TitleTop && (
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.Title.Name}
+                  </p>
+                )}
+                <h1 className="font-display text-4xl">
+                  {xivapi.Character.Name}
+                </h1>
+                {!xivapi.Character.TitleTop && (
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.Title.Name}
+                  </p>
+                )}
               </div>
               <div>
-                <h2 className="font-xivmeter text-lg">NAMEDAY</h2>
-                <p className="font-sans text-lg">{xivapi.Character.Nameday}</p>
-              </div>
-              <div>
-                <h2 className="font-xivmeter text-lg">GUARDIAN DEITY</h2>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.GuardianDeity.Name}
-                </p>
-              </div>
-              <div>
-                <h2 className="font-xivmeter text-lg">GRAND COMPANY</h2>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.GrandCompany.Company.Name}
-                </p>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.GrandCompany.Rank.Name}
-                </p>
-              </div>
-              <div>
-                <h2 className="font-xivmeter text-lg">FREE COMPANY</h2>
-                <p className="font-sans text-lg">
-                  {xivapi.Character.FreeCompanyName}
-                </p>
-              </div>
-              <div>
-                <h2 className="font-xivmeter text-lg">SERVER</h2>
-                <p className="font-sans text-lg">{xivapi.Character.Server}</p>
-              </div>
-            </div>
-            <div className="grid grid-flow-row-dense grid-cols-8 gap-2 py-2">
-              {xivapi.Character.ClassJobs.map((classJob) => (
-                <div
-                  key={classJob.UnlockedState.Name}
-                  className="flex flex-col text-center pb-1"
-                >
+                <div className="flex flex-row space-x-2 items-center pb-2">
                   <Image
-                    className="mx-auto"
-                    src={jobIcons[classJob.UnlockedState.ID] ?? jobIcons[36]}
-                    alt={classJob.UnlockedState.Name}
+                    src={
+                      jobIcons[xivapi.Character.ActiveClassJob.UnlockedState.ID]
+                    }
+                    alt={xivapi.Character.ActiveClassJob.UnlockedState.Name}
                     height={32}
                     width={32}
-                  ></Image>
+                  />
+                  <h2 className="font-display text-bold text-2xl">
+                    {xivapi.Character.ActiveClassJob.UnlockedState.Name.toUpperCase()}
+                  </h2>
+                </div>
+
+                <h2 className="font-xivmeter text-xl">
+                  LEVEL {xivapi.Character.ActiveClassJob.Level}
+                </h2>
+                <div className="h-1 min-w-max bg-stone-300 dark:bg-stone-700">
+                  <div
+                    className="h-1"
+                    style={{ width: `${xivLevelPercentage}%` }}
+                  >
+                    <Transition
+                      appear={true}
+                      show={isShowing}
+                      enter="origin-left transition duration-[700ms]"
+                      enterFrom="scale-x-0"
+                      enterTo="scale-x-100"
+                    >
+                      <div className="h-1 min-w-min bg-stone-600 dark:bg-stone-300"></div>
+                    </Transition>
+                  </div>
+                </div>
+                <p className="font-sans text-md">
+                  {xivapi.Character.ActiveClassJob.Level === 90
+                    ? "MAX"
+                    : `EXP ${xivapi.Character.ActiveClassJob.ExpLevel} / ${xivapi.Character.ActiveClassJob.ExpLevelMax}`}
+                </p>
+              </div>
+              <div className="grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-6">
+                <div>
+                  <h2 className="font-xivmeter text-lg">
+                    RACE / CLAN / GENDER
+                  </h2>
                   <p className="font-sans text-lg">
-                    {classJob.Level !== 0 ? classJob.Level : "-"}
+                    {xivapi.Character.Race.Name}
+                  </p>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.Tribe.Name}{" "}
+                    {ffxivGender[xivapi.Character.Gender]}
                   </p>
                 </div>
-              ))}
-            </div>
+                <div>
+                  <h2 className="font-xivmeter text-lg">NAMEDAY</h2>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.Nameday}
+                  </p>
+                </div>
+                <div>
+                  <h2 className="font-xivmeter text-lg">GUARDIAN DEITY</h2>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.GuardianDeity.Name}
+                  </p>
+                </div>
+                <div>
+                  <h2 className="font-xivmeter text-lg">GRAND COMPANY</h2>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.GrandCompany.Company.Name}
+                  </p>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.GrandCompany.Rank.Name}
+                  </p>
+                </div>
+                <div>
+                  <h2 className="font-xivmeter text-lg">FREE COMPANY</h2>
+                  <p className="font-sans text-lg">
+                    {xivapi.Character.FreeCompanyName}
+                  </p>
+                </div>
+                <div>
+                  <h2 className="font-xivmeter text-lg">SERVER</h2>
+                  <p className="font-sans text-lg">{xivapi.Character.Server}</p>
+                </div>
+              </div>
+              <div className="grid grid-flow-row-dense grid-cols-8 gap-2 py-2">
+                {xivapi.Character.ClassJobs.map((classJob) => (
+                  <div
+                    key={classJob.UnlockedState.Name}
+                    className="flex flex-col text-center pb-1"
+                  >
+                    <Image
+                      className="mx-auto"
+                      src={jobIcons[classJob.UnlockedState.ID] ?? jobIcons[36]}
+                      alt={classJob.UnlockedState.Name}
+                      height={32}
+                      width={32}
+                    ></Image>
+                    <p className="font-sans text-lg">
+                      {classJob.Level !== 0 ? classJob.Level : "-"}
+                    </p>
+                  </div>
+                ))}
+              </div>
 
-            <Button
-              href={
-                "https://na.finalfantasyxiv.com/lodestone/character/" +
-                characterID
-              }
-            >
-              {intl.formatMessage({ id: "View on Lodestone" })}
-            </Button>
+              <Button
+                href={
+                  "https://na.finalfantasyxiv.com/lodestone/character/" +
+                  characterID
+                }
+              >
+                {intl.formatMessage({ id: "View on Lodestone" })}
+              </Button>
+            </div>
+          </FadeIn>
+        </div>
+      ) : (
+        <div className="container mx-auto grid place-content-center">
+          <div className="py-4 px-6 bg-stone-200 dark:bg-stone-800 rotate-1 shadow-md rounded-md">
+            <p className="font-sans text-lg">
+              {intl.formatMessage({
+                id: "XIVAPI is currently unavailable. Sorry for the inconvenience!",
+              })}
+            </p>
           </div>
-        </FadeIn>
-      </div>
+        </div>
+      )}
       <FadeIn>
         <section className="mx-auto px-4 lg:container">
           <div className="grid place-content-center text-center pt-24 pb-16 gap-2">
@@ -505,7 +535,9 @@ export default function FFXIV({
 
 export const getStaticProps = async () => {
   const res = await fetch(
-    "https://xivapi.com/character/" + characterID + "?extended=1"
+    "https://xivapi.com/character/" +
+      characterID +
+      "?extended=1&private_key=262e2e1202664c97b38032efa438f17866006b77e11a4ccfad6aec87523dea19"
   );
   const xivapi: xivData = await res.json();
 
