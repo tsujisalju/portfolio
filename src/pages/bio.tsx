1aeb06dce0ae6981a66b59d9bd866a7b8d8acbaa
purrnama <purrnama@proton.me>
2024-05-04 19:27:19 +0800

    feat: add cooper illustration to bio

--- src/pages/bio.tsx
+++ src/pages/bio.tsx
@@ -15,7 +15,7 @@ export default function About() {
     <Layout>
       <div className="md:container md:mx-auto lg:max-w-4xl px-4">
         <div className="flex flex-col-reverse lg:flex-row justify-center space-x-4">
-          <div className="flex flex-col space-y-6 font-serif text-md text-justify">
+          <div className="flex flex-col space-y-4 font-serif text-md text-justify">
             <div className="container mx-auto grid place-items-center">
               <Image
                 alt="Young Van Carina among some sunflowers"
@@ -35,6 +35,15 @@ export default function About() {
                 id: "My mother introduced me to programming and game making since I was 9 years old, because I was always on the computer and I ought to put my hobby to good use rather than just mindless flash gaming. And you know what I was pretty hooked by the idea. The fact that I could create something awesome with my own touch of flare is amazing, and the passion burns strong since. The time was convenient enough that my middle school was getting around into participating robotics and game jam competitions. My friends and I pioneered the Robotics Club and I handled the programming side of things. One of the best memories I had was creating a Lego EV3 Mindstorms claw robot for an intrastate competition, and making the design decision to control it with another EV3 brick via Bluetooth instead of a phone over Wifi, which worked in our favor during the run. Before I left the school, they were already setting up courses for game dev with Unity3D, which is so super cool to see.",
               })}
             </p>
+            <div className="container mx-auto grid place-items-center">
+              <Image
+                alt="Young Cooper Baihaqi reading a journal"
+                width={300}
+                height={300}
+                src={"/img/about/cooper-journal.png"}
+                placeholder="empty"
+              />
+            </div>
             <p>
               {intl.formatMessage({
                 id: "I remember writing journals when I was in middle school. Starting Form 1, I had a notebook in which I wrote just about anything that happened one particular day, and I would also scribble drawings on the side. There was a total of about 8 books and 3 sketchbooks before I took the SPM exam. Some of them got caught in the rain while some went missing and was later found with boob vandalism on my more feminine characters. They are largely embarassing now, but they remain fragments of memories worth documenting here perpetually.",
