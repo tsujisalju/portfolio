930ce0e677c63d9f9fcaaae282ecd4877b609347
purrnama <purrnama@proton.me>
2024-06-21 23:07:42 +0800

    feat: refactor art masonry

--- src/utilities/FadeIn.tsx
+++ src/utilities/FadeIn.tsx
@@ -1,13 +1,21 @@
 import { Transition } from "@headlessui/react";
 import { ReactNode, useEffect, useState } from "react";
 
-export function FadeIn({ children }: { children: ReactNode }) {
+export function FadeIn({
+  className,
+  children,
+}: {
+  className: string;
+  children: ReactNode;
+}) {
   const [isShowing, setIsShowing] = useState(false);
   useEffect(() => {
     setIsShowing(true);
   }, []);
   return (
     <Transition
+      as="div"
+      className={className}
       show={isShowing}
       enter="transition duration-400"
       enterFrom="opacity-0"
