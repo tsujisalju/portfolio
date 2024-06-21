b97edbe137e24ad6df4e70e725409ee4d928220f
purrnama <purrnama@proton.me>
2024-06-21 23:07:42 +0800

    feat: refactor art masonry

--- src/components/project-gallery/ProjectGallery.tsx
+++ src/components/project-gallery/ProjectGallery.tsx
@@ -2,12 +2,14 @@ import ProjectGalleryItem from "./ProjectGalleryItem";
 import { Project } from "../../lib/projects";
 import React from "react";
 import { FadeIn } from "../../utilities/FadeIn";
+import Tilt from "react-parallax-tilt";
+import { FormattedDate } from "react-intl";
 
 export default function ProjectGallery({ projects }: { projects: Project[] }) {
   return (
     <section className="px-4">
       <FadeIn>
-        <div className="md:px-8 mx-auto grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-dense items-center">
+        <div className="md:px-8 mx-auto grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 auto-rows-fr grid-flow-dense">
           {projects.map((project: Project) => (
             <ProjectGalleryItem key={project.id} project={project} />
           ))}
