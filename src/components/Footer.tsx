2324203c8c4b0a50aea638b67f76c8ff59ce858d
Qayyum Yazid <purrnama@proton.me>
2025-05-21 12:15:10 +0800

    fix: footer mobile layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const intl = useIntl();
   return (
     <div className="w-full bg-black/10 flex justify-center mt-8 py-8">
-      <div className="lg:container grid grid-cols-4 py-4 space-y-2 items-center px-4">
+      <div className="lg:container grid grid-cols-2 lg:grid-cols-4 py-4 space-y-2 items-center px-4">
         <div className="flex flex-col space-y-4">
           <div className="flex flex-row space-x-2 items-center">
             <PurrnamaLogo className="h-6 w-auto" />
