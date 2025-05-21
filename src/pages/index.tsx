a6c083d45cd959362488142b4e11b5f26d77982f
purrnama <purrnama@proton.me>
2025-05-22 00:40:38 +0800

    feat: setup prettier and parallax hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import LogLink from "../components/logs/LogLink";
 import PurrnamaLogo from "../components/svg/PurrnamaLogo";
 import { Socials } from "../lib/socials";
+import Tilt from "react-parallax-tilt";
 import { liveBackgrounds } from "../lib/live";
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
@@ -121,88 +122,96 @@ export default function Home({
           <motion.div
             layout
             transition={{ layout: { duration: 0.4, ease: circOut } }}
-            className={
-              "max-w-xl flex flex-1 flex-col space-y-4 mx-16 " +
-              (liveBackgrounds[currentLive].reverse
-                ? "items-start"
-                : "items-end")
-            }
+            className={"max-w-xl mx-16"}
           >
-            <GeoDiv
-              className="flex flex-col items-start gap-8 p-12  bg-white/20 dark:bg-black/20 backdrop-blur-sm"
-              borderx
-              cornertr
-              cornersize="large"
+            <Tilt
+              tiltMaxAngleX={3}
+              tiltMaxAngleY={3}
+              tiltReverse
+              scale={1.02}
+              className={
+                "relative flex flex-1 flex-col space-y-4 " +
+                (liveBackgrounds[currentLive].reverse
+                  ? "items-start"
+                  : "items-end")
+              }
             >
-              <PurrnamaLogo className="h-8 w-auto" />
-              <div className="flex flex-col space-y-4">
-                <div>
-                  <h1 className="font-display text-4xl lg:text-5xl">
-                    {intl.formatMessage({
-                      id: "Move forward with courage, because you are not alone.",
-                    })}
-                  </h1>
-                </div>
-                <div>
-                  <p className="font-sans">
-                    {intl.formatMessage({
-                      id: "I'm Qayyum, a freelance developer and designer based in Johore, Malaysia.",
-                    })}
-                  </p>
-                  <p className="font-sans">
-                    {intl.formatMessage({
-                      id: "The world is ours to explore, design and nurture, for the betterment of all walks of life.",
-                    })}
-                  </p>
-                </div>
-                <div className="flex flex-row pt-4 space-x-3 place-items-center">
-                  <Link
-                    href={Socials.Github}
-                    aria-label="Visit Github profile"
-                    target={"_blank"}
-                    className={
-                      "text-stone-700 hover:text-stone-800 dark:text-stone-300 hover:dark:text-stone-200 transition duration-200"
-                    }
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      fill="currentColor"
-                      className="bi bi-github"
-                      viewBox="0 0 16 16"
+              <GeoDiv
+                className="flex flex-col items-start gap-8 p-12  bg-white/20 dark:bg-black/20 backdrop-blur-sm"
+                borderx
+                cornertr
+                cornersize="large"
+              >
+                <PurrnamaLogo className="h-8 w-auto" />
+                <div className="flex flex-col space-y-4">
+                  <div>
+                    <h1 className="font-display text-4xl lg:text-5xl">
+                      {intl.formatMessage({
+                        id: "Move forward with courage, because you are not alone.",
+                      })}
+                    </h1>
+                  </div>
+                  <div>
+                    <p className="font-sans">
+                      {intl.formatMessage({
+                        id: "I'm Qayyum, a freelance developer and designer based in Johore, Malaysia.",
+                      })}
+                    </p>
+                    <p className="font-sans">
+                      {intl.formatMessage({
+                        id: "The world is ours to explore, design and nurture, for the betterment of all walks of life.",
+                      })}
+                    </p>
+                  </div>
+                  <div className="flex flex-row pt-4 space-x-3 place-items-center">
+                    <Link
+                      href={Socials.Github}
+                      aria-label="Visit Github profile"
+                      target={"_blank"}
+                      className={
+                        "text-stone-700 hover:text-stone-800 dark:text-stone-300 hover:dark:text-stone-200 transition duration-200"
+                      }
                     >
-                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                    </svg>
-                  </Link>
-                  <Link
-                    href={Socials.Bluesky}
-                    aria-label="Visit Bluesky profile"
-                    target={"_blank"}
-                    className={
-                      "text-stone-700 hover:text-stone-800 dark:text-stone-300 hover:dark:text-stone-200 transition duration-100"
-                    }
-                  >
-                    <BlueSkyIcon className="" />
-                  </Link>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="18"
+                        height="18"
+                        fill="currentColor"
+                        className="bi bi-github"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                      </svg>
+                    </Link>
+                    <Link
+                      href={Socials.Bluesky}
+                      aria-label="Visit Bluesky profile"
+                      target={"_blank"}
+                      className={
+                        "text-stone-700 hover:text-stone-800 dark:text-stone-300 hover:dark:text-stone-200 transition duration-100"
+                      }
+                    >
+                      <BlueSkyIcon className="" />
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            </GeoDiv>
-            <GeoDiv
-              className="hidden lg:flex flex-col w-max gap-4 p-8 bg-white/20 dark:bg-black/20 backdrop-blur-sm"
-              border
-              cornertl
-              cornersize="normal"
-            >
-              <div className="max-w-xl w-full flex flex-1 flex-col space-y-2">
-                <h1 className="text-3xl font-display">
-                  {liveBackgrounds[currentLive].name}
-                </h1>
-                <p className="font-sans">
-                  {liveBackgrounds[currentLive].species}
-                </p>
-              </div>
-            </GeoDiv>
+              </GeoDiv>
+              <GeoDiv
+                className="hidden lg:flex flex-col w-max gap-4 p-8 bg-white/20 dark:bg-black/20 backdrop-blur-sm"
+                border
+                cornertl
+                cornersize="normal"
+              >
+                <div className="max-w-xl w-full flex flex-1 flex-col space-y-2">
+                  <h1 className="text-3xl font-display">
+                    {liveBackgrounds[currentLive].name}
+                  </h1>
+                  <p className="font-sans">
+                    {liveBackgrounds[currentLive].species}
+                  </p>
+                </div>
+              </GeoDiv>
+            </Tilt>
           </motion.div>
         </div>
         <div className="flex flex-row space-x-2 justify-center mt-16">
@@ -272,7 +281,10 @@ export default function Home({
                   style={{ objectPosition: "50% 30%" }}
                   placeholder="blur"
                   blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                    shimmer(allProjectsData[0].width, allProjectsData[0].height)
+                    shimmer(
+                      allProjectsData[0].width,
+                      allProjectsData[0].height,
+                    ),
                   )}`}
                 />
                 <div className="absolute w-full top-0 p-4 border bg-white/50 dark:bg-black/50 border-black/20 dark:border-white/20">
